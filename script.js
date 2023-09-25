50d795cf6d7df323f84451082d9cc483b21b066f
SkulZOnTheYT <92020118+SkulZOnTheYT@users.noreply.github.com>
2023-09-25 14:55:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function generateRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+function changeHeaderBackgroundColor() {
+    const header = document.querySelector('header');
+    header.style.backgroundColor = generateRandomColor();
+}
+
+
+window.addEventListener('load', changeHeaderBackgroundColor);
