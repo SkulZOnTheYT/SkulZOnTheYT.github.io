7c6c20f7d717079f475d162b9dc35b4e904ef63c
SkulZOnTheYT <92020118+SkulZOnTheYT@users.noreply.github.com>
2023-10-18 12:45:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const sidebarToggle = document.getElementById('sidebar-toggle');
-    const mobileMenu = document.querySelector('.mobile-menu');
+    const mobileMenu = document.getElementByClass('.mobile-menu');
 
     // Tambahkan event listener untuk tombol sidebar
     sidebarToggle.addEventListener('click', function() {
